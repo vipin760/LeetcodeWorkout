9fa45f2af7873ae9becdb69234eac8c623fa1697
vipin m <vipinm500@gmail.com>
2023-09-21 12:54:53 +0530

    roman to intiger questionNo:9

--- javascript.js
+++ javascript.js
@@ -1,18 +1,28 @@
-function number(num){
-    let res = num
-    let sum = 0
-
-  while(num>0){
-    var rem = num%10
-    sum = (sum*10)+rem
-    num = Math.floor(num/10) 
-
-  }
-  if(sum===res){
-   return true
-  }else{
-    return false
-  }
+var romanToInt = function(s) {
+    var roman = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let curVal = 0;
+    let preVal = 0;
+    let result = 0;
+    for(let i=s.length-1 ; i>=0 ; i--){
+        let romanVal = s[i]
+        curVal = roman[romanVal]
+        if(curVal=>preVal){
+            result += curVal
+    }else{
+        result -= curVal
+    }
+    preVal = curVal
 }
+   return result 
+};
 
-console.log(number(-445544));
+const input = "III"
+console.log(romanToInt(input));
